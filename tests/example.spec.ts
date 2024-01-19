b526f8ea7d9c4c57d9d26e857d3b8bb0ea6028fc
andrius <andriusvetrenka@session.id>
2024-01-19 17:26:06 +0200

    First code

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,33 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://getipass.com/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await page.locator('//button[@id="landing_logIn"]').click();
+  await page.locator('//input[@id="_com_liferay_login_web_portlet_LoginPortlet_usernameFromP"]').fill('kmsexpressinc');
+  await page.locator('//input[@id="_com_liferay_login_web_portlet_LoginPortlet_password-field"]').fill('Emma@1201');
+  await page.locator('//button[@id="loginBtn"]').click();
+  await page.waitForSelector('//button[@id="overView_popup_notNow_btn"]', { timeout: 20000 })
+  await page.locator('//button[@id="overView_popup_notNow_btn"]').click();
+  await page.locator('//*[@data-testid="NotificationsOutlinedIcon"]').click();
+  await page.locator('//button[@id="Messages_tab"]').click();
+  await page.locator('//button//span[text()=\'Download\']').first().click()
+
+  const downloadPromise = page.waitForEvent('download');
+
+
+  const download = await downloadPromise;
+
+  await download.saveAs('./' + download.suggestedFilename());
+
+
+
+
+
+
+
+
+
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
