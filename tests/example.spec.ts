f22166ae2eb84ffc88adbe8fd7d8656ad014073d
vytautaslesciauskas <152288423+vytautaslesciauskas@users.noreply.github.com>
2024-03-29 14:18:18 +0200

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -9,7 +9,7 @@ test('has title', async ({ page }) => {
   await page.locator('//input[@id="_com_liferay_login_web_portlet_LoginPortlet_usernameFromP"]').fill('kmsexpressinc');
   await page.locator('//input[@id="_com_liferay_login_web_portlet_LoginPortlet_password-field"]').fill('Emma@1201');
   await page.locator('//button[@id="loginBtn"]').click();
-  await page.waitForSelector('//button[@id="overView_popup_notNow_btn"]', { timeout: 20000 })
+  await page.waitForTimeout(5000);
   await page.locator('//button[@id="overView_popup_notNow_btn"]').click();
   await page.locator('//*[@data-testid="NotificationsOutlinedIcon"]').click();
   await page.locator('//button[@id="Messages_tab"]').click();
